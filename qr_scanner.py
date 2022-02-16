f2cdc5e2121bfcc44b3678f42241df50a5216321
jendlrs <jensennics.dlrs@gmail.com>
2022-02-16 19:14:22 +0800

    I imported the pyzbar to decode the image easily and I successfully save the infomation from qr code to a new text file.

--- qr_scanner.py
+++ qr_scanner.py
@@ -15,6 +15,7 @@
 # 	- Create a demo of your program (1-2 min) and send it directly to my messenger.
 
 import cv2
+from pyzbar import pyzbar 
 
 #Opening the webcam by using opencv
 
@@ -24,12 +25,22 @@ def detect():
 
     while True:
         _, img = webcam.read()
+        img = decode (cv2.resize (img, None, fx =1.0, fy=1.0, interpolation = cv2.INTER_AREA))
         cv2.imshow ("QR CODE SCANNER", img)
-        if cv2.waitKey (1) & 0xFF  == ord("q"):
+        if cv2.waitKey (1) ==ord ("p"):
             break
 
     webcam.release
     cv2.destroyAllWindows()
+
+def decode (img):
+    Qr_code = pyzbar.decode(img)
+    for info in Qr_code: #This loop is for decoding the qr code
+        #utf-8 or Unicode Transformation Format -8 is used for encoding the info from qr code into another file
+        Qr_data = info.data.decode ("utf-8")
+        with open ("qr_result.txt", "w") as textfile:  #It will automatically open a text file
+            textfile.write (Qr_data)                   #Writing the data from qr code to the new text file
+    return img
 
 #call the function
 detect()
