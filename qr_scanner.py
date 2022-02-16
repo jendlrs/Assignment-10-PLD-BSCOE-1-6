ba03884cf954bdb06d8cb1f15fdebf43ab64cbae
jendlrs <jensennics.dlrs@gmail.com>
2022-02-16 20:45:45 +0800

    The program is completed and working fine; I just added some comments.

--- qr_scanner.py
+++ qr_scanner.py
@@ -28,7 +28,7 @@ def detect():
         _, img = webcam.read()
         img = decode (cv2.resize (img, None, fx =1.0, fy=1.0, interpolation = cv2.INTER_AREA))
         cv2.imshow ("QR CODE SCANNER", img)
-        if cv2.waitKey (1) ==ord ("p"):
+        if cv2.waitKey (1) ==ord ("p"): #Keybind to close the scanner after reading the qr code
             break
 
     webcam.release
@@ -37,19 +37,19 @@ def detect():
 def decode (img):
     Qr_code = pyzbar.decode(img)
     for info in Qr_code: #This loop is for decoding the qr code
-        left, top, width, height = info.rect #
+        left, top, width, height = info.rect 
         cv2.rectangle (img, (left,top), (left + width, top + height), (0,0,255), 4) #this will make a red border
         #utf-8 or Unicode Transformation Format -8 is used for encoding the info from qr code into another file
         Qr_data = info.data.decode ("utf-8")
 
         #for date and time
-        real_time = datetime.now()
-        current_date = real_time.strftime ("%B/%d/%Y")
-        current_time = real_time.strftime ("%I:%M %p")
+        real_time = datetime.now() #Current time and date
+        current_date = real_time.strftime ("%B/%d/%Y") #Month/Date/Year
+        current_time = real_time.strftime ("%I:%M %p") #Hour:Minutes AM/PM
 
         with open ("qr_result.txt", "w") as textfile:  #It will automatically open a text file
             textfile.write (Qr_data)                   #Writing the data from qr code to the new text file
-            textfile.write ("Date and Time Record: " + (current_date) + " " + (current_time))
+            textfile.write ("Date and Time Record: " + (current_date) + " " + (current_time)) #Displayig date and time
 
     return img
 
